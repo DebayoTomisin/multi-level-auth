f26c73d1bc4a48919a5877bcc57d8f0df99e5b3e
Debayo Tomisin <debayotomisin07@gmail.com>
2023-02-15 07:45:25 +0100

    chore/update the login page

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import LoginColorPicker from "../src/components/auth/LoginColorPicker";
 import LoginForm from "../src/components/auth/LoginForm";
+import LoginCaptcha from "../src/components/auth/LoginCaptcha";
 import { useAppContext } from "../src/utils/context";
 
 function Login() {
   const { state } = useAppContext();
 
-  return (
-    <div>{state?.authStage == 0 ? <LoginForm /> : <LoginColorPicker />}</div>
-  );
+  function Render() {
+    switch (state?.authStage) {
+      case 2:
+        return <LoginForm />;
+
+      case 1:
+        return <LoginColorPicker />;
+
+      case 0:
+        return <LoginCaptcha />;
+      default:
+        return <LoginForm />;
+    }
+  }
+
+  return <div>{<Render />}</div>;
 }
 
 export default Login;
